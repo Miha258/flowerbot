388dea39c50bfef4a13c5759732054d2ce075820
Miha258 <77239046+Miha258@users.noreply.github.com>
2021-08-23 14:18:29 +0300

    Add files via upload

--- oclock.py
+++ oclock.py
@@ -1,19 +1,18 @@
-from asyncio.windows_events import WindowsProactorEventLoopPolicy
-import os
-from typing import no_type_check_decorator
+
+
 
 
 class Clock:
 
   __DAY = 86400
   def __init__(self,secs:int):
-   self.__secs = secs % self.__DAY
+      self.__secs = secs % self.__DAY
 
   def get_time(self):
-  	s = self.__secs % 60
-  	m = (self.__secs // 60) % 60
-  	h = (self.__secs // 3600) % 24
-  	return h,m,s
+      s = self.__secs % 60
+      m = (self.__secs // 60) % 60
+      h = (self.__secs // 3600) % 24
+      return h,m,s
 
 
 
