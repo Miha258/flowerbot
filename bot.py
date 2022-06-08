da1e38aadcccdcd52f1bc38cc2f58ef47a93bed8
Miha258 <77239046+Miha258@users.noreply.github.com>
2022-06-08 15:19:30 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ import requests
 from PIL import Image,ImageFont,ImageDraw
 from dashboard import draw_progress
 import pytz
-
+from db import create_tables
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix = '!',intents = intents)
@@ -29,94 +29,7 @@ cursor = conection.cursor()
 #add member to db
 async def on_ready():
   print('Я готов!')
- 
-  cursor.execute("""CREATE TABLE IF NOT EXISTS serverss_db (
-      name TEXT,
-      id INT,
-      cash BIGINT,
-      timess INT,
-      when_climed INT,
-      names TEXT,
-      private_room INT
-      )""")
-  conection.commit()
-  cursor.execute("""CREATE TABLE IF NOT EXISTS log (
-      id INT,
-      timess TEXT,
-      resaon TEXT
-  )""")
-  conection.commit()
-  cursor.execute("""CREATE TABLE IF NOT EXISTS adminnsnff (
-      id INT,                                      
-      reports INT,
-      mutes INT,
-      warns INT,
-      weekly_mutes INT,
-      weekly_reports INT,
-      weekly_warns INT,
-      rating_pos INT,
-      rating_neg INT,
-      weekly_clear INT 
-      )""")
-  conection.commit()
-  cursor.execute("""CREATE TABLE IF NOT EXISTS warninform (
-      name TEXT,
-      id INT,
-      warns INT,
-      type TEXT,
-      reason TEXT,
-      timess TEXT,
-      admin INT
-      )""") 
-  conection.commit()
-  
-  cursor.execute("""CREATE TABLE IF NOT EXISTS shops (
-      guild_id INT,
-      role_name TEXT,
-      role_id INT,
-      cost BIGINT
-      
-  )""")
-  conection.commit()
- 
-  cursor.execute("""CREATE TABLE IF NOT EXISTS exp_system  (
-      xp BIGINT,
-      lvl INT,
-      when_xp INT,
-      id INT
-      
-  )""")
-  conection.commit()
-  
-  cursor.execute("""CREATE TABLE IF NOT EXISTS clans  (
-      clan_name BIGINT,
-      clan_owner TEXT,
-      clan_id INT,
-      clan_members INT,
-      in_clan BOOLEAN,
-      have_clan BOOLEAN,
-      when_created TEXT,
-      users INT,
-      clan_channel_id INT,
-      clan_text_channel_id INT,
-      clan_role_id INT,
-      clan_slots INT,
-      time_for_pay INT,
-      str_time_for_pay TEXT,
-      clan_img_url TEXT,
-      clan_avatar_url TEXT,
-      clan_description TEXT,
-      has_invited BOOLEAN,
-      channels INT,
-      clan_points INT,
-      has_payed BOOLEAN,
-      clan_cash BIGINT,
-      lvl INT,
-      balance INT,
-      voice_time INT
-
-      
-  )""")
+  create_tables()
   conection.commit()
   
 
